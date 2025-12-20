bd948df188cc1bbf055183b5c6cc2af4e1253922
Leumars-o <samex150@gmail.com>
2025-12-20 11:59:48 +0100

    Export NotificationService in services module

--- services/__init__.py
+++ services/__init__.py
@@ -9,6 +9,7 @@ Each service handles a specific domain:
 - TokenService: Token detection and info fetching
 - TransferService: Blockchain transfers
 - LimitOrderService: Limit sell order management
+- NotificationService: Admin notifications to channels/groups
 """
 
 from .data_manager import DataManager
@@ -17,6 +18,7 @@ from .balance_service import BalanceService
 from .token_service import TokenService
 from .transfer_service import TransferService
 from .limit_order_service import LimitOrderService
+from .notification_service import NotificationService
 
 __all__ = [
     'DataManager',
@@ -25,4 +27,5 @@ __all__ = [
     'TokenService',
     'TransferService',
     'LimitOrderService',
+    'NotificationService',
 ]
